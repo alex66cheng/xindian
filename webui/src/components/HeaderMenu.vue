75d268c84ed2633eda46af6abaaa857de4380912
Mark Lai <laimark900317@gmail.com>
2021-07-27 20:03:57 +0800

    增加改語言功能

--- webui/src/components/HeaderMenu.vue
+++ webui/src/components/HeaderMenu.vue
@@ -9,18 +9,18 @@
     active-text-color="#ffd04b"
     text-align="left"
     style = "height:60px; width: 100%; padding-left: 20px;padding-right: 20px; border-bottom:0px;">
-    <el-menu-item index="1" >Processing Center</el-menu-item>
+    <el-submenu index="2-4">
+      <template slot="title">language</template>
+      <el-menu-item @click="changeLang('cn')">cn</el-menu-item>
+      <el-menu-item @click="changeLang('jp')">jp</el-menu-item>
+      <el-menu-item index="2-4-3">item three</el-menu-item>
+    </el-submenu>
     <el-submenu index="2">
       <template slot="title">Workspace</template>
       <el-menu-item index="2-1">item one</el-menu-item>
       <el-menu-item index="2-2">item two</el-menu-item>
       <el-menu-item index="2-3">item three</el-menu-item>
-      <el-submenu index="2-4">
-        <template slot="title">item four</template>
-        <el-menu-item index="2-4-1">item one</el-menu-item>
-        <el-menu-item index="2-4-2">item two</el-menu-item>
-        <el-menu-item index="2-4-3">item three</el-menu-item>
-      </el-submenu>
+      
     </el-submenu>
     <el-menu-item index="3" disabled>Info</el-menu-item>
     <el-menu-item index="4"><a href="https://www.ele.me" target="_blank">Orders</a></el-menu-item>
@@ -71,6 +71,9 @@ export default {
         }
       }
     },
+    changeLang(lang){
+      this.$i18n.locale = lang
+    }
     
     
   }
