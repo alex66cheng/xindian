c447a78b3ef29ce67a618cb3b8612454ed6d6558
YJack0000 <jackleagends@gmail.con>
2021-07-29 03:35:22 +0800

    改變 home 往其他路徑的方式 (router.push)

--- webui/src/views/Home.vue
+++ webui/src/views/Home.vue
@@ -1,14 +1,14 @@
 <template>
   <div>
     <h1> Welcome! </h1>
-    <el-button type="primary" onclick="document.location.href='/monitor'">導向 monitor</el-button>
+    <el-button type="primary" @click="toMonitor">導向 monitor</el-button>
     <el-row gutter="20">
       <el-col :span="8" :offset="3">
         <el-card :body-style="{ padding: '50px' }">
           <img class="big-logo image" src="../assets/img/KASO_logo.png">
           <div style="padding: 14px;">
             <span>挖喜 KASO</span>
-            <el-button type="primary" class="button">登入</el-button>
+            <el-button type="primary" class="button" @click="toLogin">登入</el-button>
           </div>
         </el-card>
       </el-col>
@@ -17,13 +17,27 @@
           <img class="big-logo image" src="../assets/img/OHGA_logo.jpg">
           <div style="padding: 14px;">
             <span>挖喜 OHGA</span>
-            <el-button type="primary" class="button">登入</el-button>
+            <el-button type="primary" class="button" @click="toLogin">登入</el-button>
           </div>
         </el-card>
       </el-col>
     </el-row>
   </div>
 </template>
+
+<script>
+export default{
+ methods:{
+    toLogin(){
+      this.$router.push({name: 'login'})
+    },
+    toMonitor(){
+      this.$router.push({name: 'monitor'})
+    }
+ }
+}
+</script>
+
 
 <style scoped>
   .big-logo{
