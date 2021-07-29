61a9821f19371775b5f17a57e30c2f1cc9a31a9a
YJack0000 <jackleagends@gmail.con>
2021-07-30 04:04:16 +0800

    增加搜尋功能 增加動態搜尋 重寫整體結構成 table

--- webui/src/components/DeviceManager.vue
+++ webui/src/components/DeviceManager.vue
@@ -1,40 +1,70 @@
 <template>
-<div>
-  <el-row>
-    <el-col :span="4">
-      <div class="device-parameter">
-        <el-tag>S/N</el-tag>
-        <div class="device-value">{{SN}}</div>
-      </div>
-    </el-col>
-    <el-col :span="4">
-      <div class="device-parameter">
-        <el-tag>Type</el-tag>
-        <div class="device-value">{{type}}</div>
-      </div>
-    </el-col>
-    <el-col :span="4">
-      <div class="device-parameter">
-        <el-tag>Status</el-tag>
-        <div class="device-value">{{status}}</div>
-      </div>
-    </el-col>
-    <el-col :span="4">
-      <div class="button-configModbus">
-        <el-button @click="gotoConfig">config</el-button>
-      </div>
-
-    </el-col>
-    <el-col :span="4">
-      <div class="">
-        <el-button @click="gotoConfig">modbus</el-button>
-      </div>
-    </el-col>
-    <el-col :span="4">
-    </el-col>
-  
-  </el-row>
-</div>
+  <div style="padding: 20px;">
+    <el-row gutter="10">
+      <el-col span="2">
+        <el-button type="primary">新增</el-button>
+      </el-col>
+      <el-col span="2">
+        <el-button >刪除</el-button>
+      </el-col>
+      <el-col span="20" align="right">
+        <el-input
+          scope
+          style="width: 240px;"
+          v-model="search"
+          placeholder="輸入關鍵字搜尋"
+        />
+      </el-col>
+    </el-row>
+    <el-row>
+      <el-col>
+        <el-table
+          :data="tableData"
+          height="100%"
+          stripe
+          style="width: 100%;">
+          <el-table-column 
+            prop="select"
+            type="selection" 
+            :reserve-selection="true" 
+            width="50">
+          </el-table-column>
+          <el-table-column
+            prop="id"
+            label="ID"
+            width="90">
+          </el-table-column>
+          <el-table-column
+            prop="sn"
+            label="S/N"
+            width="180">
+          </el-table-column>
+          <el-table-column
+            prop="type"
+            label="Type"
+            width="180">
+          </el-table-column>
+          <el-table-column
+            prop="status"
+            label="Status">
+            <template slot-scope="scope">
+              <el-switch active-color="#13ce66" inactive-color="#ff4949" v-model="scope.row.status" @change=changeStatus(scope.row)>
+              </el-switch>
+            </template>
+          </el-table-column>
+          <el-table-column
+            fixed="right"
+            label="操作"
+            width="100">
+            <template slot-scope="scope">
+              <el-button @click="gotoConfig(scope.row.sn)" type="text" size="small">查看</el-button>
+              <el-button @click="gotoConfig(scope.row.sn)" type="text" size="small">编辑</el-button>
+            </template>
+          </el-table-column>
+        </el-table>
+      </el-col>
+    </el-row>
+  </div>
 </template>
 
 <script>
@@ -43,15 +73,46 @@
 export default {
     data(){
       return{
-        SN: 'KA-00001',
-        type: '700',
-        status: 'active'
-
+        search: '',
+        deviceData:[
+          {
+            id: 1,
+            sn: 'KA-00001',
+            type: '700',
+            status: true
+          },
+          {
+            id: 2,
+            sn: 'KA-00001',
+            type: '700',
+            status: true
+          }
+        ]
+      }
+    },
+    computed: {
+      // 實時監聽表格
+      tableData: function() {
+          const search = this.search
+          if (search) {
+              return this.deviceData.filter(deviceData => {
+                  return Object.keys(deviceData).some(key => {
+                      return String(deviceData[key]).toLowerCase().indexOf(search) > -1
+                  })
+              })
+          }
+          return this.deviceData
       }
     },
     methods:{
-      gotoConfig(){
-        this.$router.push({name:'gatewayConfig', params:{device: this.SN}})
+      gotoConfig(target_sn){
+        this.$router.push({name:'gatewayConfig', params:{device: target_sn}})
+      },
+      changeStatus(row){ // 到時候修該 status 要用到
+        const data = {
+          id: row.id,
+          status: row.status,
+        }
       }
     }
 
