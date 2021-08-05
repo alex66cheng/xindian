583bc98e0ff156ab27905d4c232b29010f3da489
YJack0000 <jackleagends@gmail.con>
2021-08-06 03:32:05 +0800

    dashboard path 變成吃活 id

--- webui/src/router/DashboardPath.js
+++ webui/src/router/DashboardPath.js
@@ -1,52 +1,19 @@
-
 import DashboardFrame from '../components/frames/DashboardFrame.vue'
-//import Dashboard from '../components/Dashboard.vue'
-//import i18n from '../lang/lang.js'
-import Dashboard601 from '../components/gatewayRoom/Dashboard601'
-import Dashboard602 from '../components/gatewayRoom/Dashboard602'
-import Dashboard603 from '../components/gatewayRoom/Dashboard603'
+import Dashboard from '../components/gatewayRoom/Dashboard'
+
 const routes = {
-  path: 'dashboard',  
-  
+  path: 'dashboardManager',
   component: DashboardFrame,
   children:[
-    /*{
-      path:'',
+    {
+      path: 'dashboard/:id',
       component: Dashboard,
-      meta:{
-        title: i18n.messages[i18n.locale]['Dashboard_Title']//只要改 Dashboard_title
-        
-      }
-    },*/
-    {
-      path:'601',
-      name:'601',
-      component: Dashboard601,
-      meta:{
-        title: '601',
-        closable: false
-      }
-    },
-    {
-      path:'602',
-      name:'602',
-      component: Dashboard602,
-      meta:{
-        title: '602',
-        closable: false
-      }
-    },
-    {
-      path:'603',
-      name:'603',
-      component: Dashboard603,
-      meta:{
-        title: '603',
-        closable: false
+      meta: {
+        title: route => `${route.params.id}`,
+        key: 'path'
       }
     }
-    
-  ]   
+  ]
 }
 
 const dashboardPath = [routes]
