f0c05f862773b17052fafd939a1a163874d23b5d
YJack0000 <jackleagends@gmail.con>
2021-08-06 03:33:00 +0800

    小修

--- webui/src/views/Home.vue
+++ webui/src/views/Home.vue
@@ -1,18 +1,11 @@
 <template>
   <div>
     <h1> Welcome! </h1>
-    <el-button type="primary" @click="toMonitor">導向 monitor</el-button>
+    <el-button type="primary" @click="$router.push({name: 'login'})">導向 monitor</el-button>
     <el-row :gutter="10">
       <el-col :span="8" :offset="8">
-        <el-card :body-style="{ padding: '50px' }">
-          <img class="big-logo image" src="../assets/img/KASO_logo.png">
-          <div style="padding: 14px;">
-            <span>挖喜 KASO</span>
-            <el-button type="primary" class="button" @click="toLogin">登入</el-button>
-          </div>
-        </el-card>
+      <el-button type="primary" class="button" @click="$router.push({name: 'login'})">登入</el-button>
       </el-col>
-      
     </el-row>
   </div>
 </template>
@@ -20,12 +13,7 @@
 <script>
 export default{
  methods:{
-    toLogin(){
-      this.$router.push({name: 'login'})
-    },
-    toMonitor(){
-      this.$router.push({name: 'monitor'})
-    }
+    
  }
 }
 </script>
