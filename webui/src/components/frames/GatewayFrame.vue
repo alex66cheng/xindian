273cc48b22ecd974acddbf7873be7c4942d4d499
YJack0000 <jackleagends@gmail.com>
2021-08-06 17:44:13 +0800

    有 i18n 之後就不用用 tabs 了

--- webui/src/components/frames/GatewayFrame.vue
+++ webui/src/components/frames/GatewayFrame.vue
@@ -1,5 +1,5 @@
 <template>
-  <router-tab :tabs="['/monitor/deviceManager']" style="height: 100%" class="drop-shadow"/>
+  <router-tab style="height: 100%" class="drop-shadow"/>
 </template>
 
 <style scoped>
