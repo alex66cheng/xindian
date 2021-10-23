b7c1ffc6d7b4506f6aa1137b10a77890dac06bb2
Mark Lai <laimark900317@gmail.com>
2021-10-23 15:02:49 +0800

    修改index.js防止為登入的使用者存取

--- webui/src/router/index.js
+++ webui/src/router/index.js
@@ -28,7 +28,7 @@ import i18n from '../lang/lang.js'
 import DashboardPath from './DashboardPath.js'
 import DevicePath from './DevicePath.js'
 
-//import firebase from 'firebase/app' 防止重整logout
+import firebase from 'firebase/app' //防止重整logout
 import 'firebase/auth'
 
 Vue.use(Router)
@@ -150,14 +150,14 @@ const router = new Router({
   ]
 })
 
-/*router.beforeEach((to, from, next)=>{
+router.beforeEach((to, from, next)=>{
   const requiresAuth = to.matched.some(record => record.meta.requiresAuth)
   const isAuthenticated = firebase.auth().currentUser
   if(requiresAuth && !isAuthenticated){
-    next('/login')
+    next({name:'login', params:{lang: 'jp'}})
   }else{
     next()
   }
-})*/
+})
 
 export default router
