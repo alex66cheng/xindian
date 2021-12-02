4ae60ef6ffb5a20a2351afa93995f69aa71524ee
YJack <jackleagends@gmail.com>
2021-12-02 13:49:43 +0800

    讀機器資料出來到旁邊的 card

--- webui/src/components/GatewayConfig.vue
+++ webui/src/components/GatewayConfig.vue
@@ -1,37 +1,65 @@
 <template>
-  <el-contianer>
-    <el-table
-    :data='deviceData'
-    style='width: 100%'>
-      <el-table-column type='expand'>
-        <template slot-scope='props'>
-          <el-container style="display: flex">
-          <el-container style="width: 50%">
-            <el-form label-position='left' inline class='demo-table-expand'>
-              <el-form-item v-for="(item, key) in props.row.interface" :key="key" :label=key style="width: 100%">
-                <span> {{ item }} </span>
-              </el-form-item>
-            </el-form>
-          </el-container>
-          <el-container style="width: 50%">
-            <span>param table</span>
-          </el-container>
-          </el-container>
-        </template>
-      </el-table-column>
-      <el-table-column
-        label='ID'
-        prop='id'>
-      </el-table-column>
-      <el-table-column
-        label='Device'
-        prop='interface.dev'>
-      </el-table-column>
-      <el-table-column
-        label='Protocal'
-        prop='protocal.type'>
-      </el-table-column>
-    </el-table>
+  <el-contianer style="display: flex">
+    <el-container style="width: 30%">
+      <el-card class="box-card">
+        <div slot="header" class="clearfix">
+          <span> {{ deviceInfo.my.id }} </span>
+          <el-button style="float: right; padding: 3px 0" type="text">操作按钮</el-button>
+        </div>
+        <div v-for="(param, index) in deviceInfo.mqtt" :key="index" class="text item" style="float: left; ">
+          {{index + ": " + param }}
+        </div>
+      </el-card>
+    </el-container>
+    <el-container style="width: 60%; display: block; padding: 20px">
+      <el-container style="width: 100%">
+        <el-button type="primary">添加</el-button>
+        <el-button type="danger">刪除</el-button>
+      </el-container>
+      <el-table
+      :data='deviceData'
+      style='width: 100%;'>
+        <el-table-column type='expand'>
+          <template slot-scope='props'>
+            <el-container style="display: flex">
+              <el-container style="width: 50%">
+                <el-form label-position='left' inline class='demo-table-expand'>
+                  <el-form-item v-for="(item, key) in props.row.protocal" :key="key" :label=key style="width: 100%">
+                    <span> {{ item }} </span>
+                  </el-form-item>
+                </el-form>
+              </el-container>
+              <el-container style="width: 50%">
+                <span>param table</span>
+              </el-container>
+            </el-container>
+          </template>
+        </el-table-column>
+        <el-table-column
+          label='ID'
+          prop='id'>
+        </el-table-column>
+        <el-table-column
+          label='Device'
+          prop='interface.dev'>
+        </el-table-column>
+        <el-table-column
+          label='Protocal'
+          prop='protocal.type'>
+        </el-table-column>
+        <el-table-column>
+          <template slot-scope="scope">
+            <el-button
+              size="mini"
+              @click="handleEdit(scope.$index, scope.row)">Edit</el-button>
+            <el-button
+              size="mini"
+              type="danger"
+              @click="handleDelete(scope.$index, scope.row)">Delete</el-button>
+          </template>
+        </el-table-column>
+      </el-table>
+    </el-container>
   </el-contianer>
 </template>
 
@@ -44,6 +72,16 @@ export default {
   },
   data(){
     return{
+      deviceInfo: {
+        'my': {
+          'id': 'KA0000001'
+        },
+        'mqtt': {
+          'ip': '52.197.39.218',
+          'client': 'kaso8467',
+          'port': '1883'
+        }
+      },
       deviceData: [
         {
           'id':'1',
@@ -356,8 +394,12 @@ export default {
     }
   },
   methods: {
-    
-    
+    handleEdit(index, row) {
+        console.log(index, row)
+    },
+    handleDelete(index, row) {
+      console.log(index, row)
+    }
   },
   created(){
     this.device = this.$route.params.device
@@ -377,5 +419,25 @@ export default {
     margin-right: 0;
     margin-bottom: 0;
     width: 50%;
+  }
+  .text {
+    font-size: 14px;
+  }
+
+  .item {
+    margin-bottom: 18px;
+  }
+
+  .clearfix:before,
+  .clearfix:after {
+    display: table;
+    content: "";
+  }
+  .clearfix:after {
+    clear: both
+  }
+
+  .box-card {
+    width: 480px;
   }
 </style>
