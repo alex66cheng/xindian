aac56c3ff581455840ef7a1e579e83a923e26305
YJack <jackleagends@gmail.com>
2021-12-16 00:46:42 +0800

    interface 裡面的 edit 可以改 state 裡面的數值

--- webui/src/components/Interface/InterfaceEditForm.vue
+++ webui/src/components/Interface/InterfaceEditForm.vue
@@ -1,8 +1,10 @@
 <template>
 <el-form>
-    <el-button  @click="editable = !(editable)" style="">Edit</el-button>
-    <el-form-item v-for="(value, index) in this.interface" :key="index" :label="index">
-        <el-input :disabled="editable" v-model="interface[index]"></el-input>
+    <el-button v-if="!(editable)" @click="editable = !(editable)" style="">Edit</el-button>
+    <el-button v-if="editable" @click="editable = !(editable)" style="">Done</el-button>
+    <el-button v-if="editable" @click="confirmEdit" type="primary">Confirm</el-button>
+    <el-form-item v-for="(value, index) in this.interfaceFormData" :key="index" :label="index">
+        <el-input :disabled="!(editable)" v-model="interfaceFormData[index]"></el-input>
     </el-form-item>
 </el-form>
 </template>
@@ -11,19 +13,33 @@
 export default{
     data(){
         return {
-            id: '',
-            interface: {},
-            editable: false
+            editable: false,
+            interfaceFormData: {}
         }
     },
     props:{
-        InterfaceData: Object
+        InterfaceId: String
+    },
+    computed: {
+        interfaceFormDataFromStatus: function(){
+            //console.log(this.InterfacceId)
+            //console.log(this.$store.state.config.all)
+            return this.$store.state.config.all[this.InterfaceId].interface
+        }
     },
     created(){
-        this.id = this.InterfaceData.id
-        this.interface = this.InterfaceData.interface
+        this.interfaceFormData = this.interfaceFormDataFromStatus
     },
     methods:{
+        confirmEdit(){
+            this.editable = !(this.editable)
+            console.log('new interface data: ', JSON.stringify(this.interfaceFormData))
+            console.log('call mutation "setInterface"')
+            this.$store.commit('setInterface', {
+                id: this.InterfaceId,
+                data: this.interfaceFormData
+            })
+        }
     }
 }
 </script>
