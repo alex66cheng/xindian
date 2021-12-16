cbc92a34baa02340642ad8ef96c452ca8b63d4c9
Mark Lai <laimark900317@gmail.com>
2021-12-16 15:58:16 +0800

    修復追加不見

--- webui/src/components/Interface/device/DeviceTable.vue
+++ webui/src/components/Interface/device/DeviceTable.vue
@@ -36,7 +36,7 @@
       </el-table-column>
 
       <el-table-column>
-        <template slot="header">
+        <template slot="header" slot-scope="scope">
           <el-button @click="centerDialogVisible = true">{{$t("add")}}</el-button>
         </template>
         <template slot-scope="scope">
